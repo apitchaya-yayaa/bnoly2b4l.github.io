c8baceb6c53aff112599eb6c3b718ce3a86aac2f
Apitchaya Denthaworn <bnoly_2b4l@hotmail.com>
2020-02-06 22:43:16 +0700

    Updates

---
+++ precache-manifest.6be5c13e873ce90815b12b91126b20b4.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24d6c0c44d480ee49e06a7982d448e21",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bf3d140df4483f2565b8",
+    "url": "/static/css/2.ddb6f80b.chunk.css"
+  },
+  {
+    "revision": "bf3d140df4483f2565b8",
+    "url": "/static/js/2.d7d0693a.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/static/js/2.d7d0693a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7b54a8b6cf94e57232ab",
+    "url": "/static/js/main.b503f8d3.chunk.js"
+  },
+  {
+    "revision": "96baba19d501e89eefb9",
+    "url": "/static/js/runtime-main.ec64c261.js"
+  },
+  {
+    "revision": "90f1a2fd6db67e8ee675e8d60d5a3e26",
+    "url": "/static/media/arrow.90f1a2fd.svg"
+  },
+  {
+    "revision": "d9a74238089e25f16990f16881dc88d5",
+    "url": "/static/media/coding.d9a74238.svg"
+  },
+  {
+    "revision": "d99368ca4e133a9917838e76243e8cfb",
+    "url": "/static/media/facebook.d99368ca.svg"
+  },
+  {
+    "revision": "5b23a837268b09d0efa9e307023c1dae",
+    "url": "/static/media/instagram.5b23a837.svg"
+  },
+  {
+    "revision": "37acdaaacca871a9a19c43d0695e2cde",
+    "url": "/static/media/mail.37acdaaa.svg"
+  },
+  {
+    "revision": "ea08834440ea85c3f08b84ce0b6622f9",
+    "url": "/static/media/metromerce.ea088344.png"
+  },
+  {
+    "revision": "9021e879bcbff87b763b574e0de1eda5",
+    "url": "/static/media/portfolio.9021e879.svg"
+  },
+  {
+    "revision": "23dfcf83a7076b6cfe95a2367badecc5",
+    "url": "/static/media/practicum.23dfcf83.jpg"
+  },
+  {
+    "revision": "d376b9252fa1075b12ab524ba9d7b325",
+    "url": "/static/media/profile.d376b925.jpg"
+  },
+  {
+    "revision": "7fe3c43b41777b9de81409b8d394b524",
+    "url": "/static/media/protectsugar.7fe3c43b.png"
+  },
+  {
+    "revision": "9ce3b0db25061531076d425b5427a3c9",
+    "url": "/static/media/rebel.9ce3b0db.png"
+  },
+  {
+    "revision": "aad62586fddf58f8ae5a7a83d1520503",
+    "url": "/static/media/resume.aad62586.pdf"
+  },
+  {
+    "revision": "735ddf04150ad4204dad7512af088dd3",
+    "url": "/static/media/spacegame.735ddf04.png"
+  },
+  {
+    "revision": "664238b5754418796f61e14ede7a7f81",
+    "url": "/static/media/tedxKU.664238b5.jpg"
+  },
+  {
+    "revision": "42853ed34724a13c6eb6fe83d61956a0",
+    "url": "/static/media/tools.42853ed3.svg"
+  },
+  {
+    "revision": "2ad1c22e298e7cb335a2f43244660679",
+    "url": "/static/media/web.2ad1c22e.svg"
+  },
+  {
+    "revision": "f69ad85afb31fdb79e2bc6a356b45985",
+    "url": "/static/media/wizardJam.f69ad85a.jpg"
+  },
+  {
+    "revision": "a171c2a03fb80e6439ad586cf04b019a",
+    "url": "/static/media/ysetter.a171c2a0.jpg"
+  }
+]);
